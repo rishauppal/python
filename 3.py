3e3b1239a46e53ead5a1236e0cd554cc45d4c160
rishauppal <rishauppal2@gmail.com>
2024-06-27 15:33:15 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,13 @@
+matrix=[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+]
+new=[]
+for i in range(4):
+    new_row=[]
+    for matrix_row in matrix:
+        new_row.append(matrix_row[i])
+    new.append(new_row)
+
+print(new)
