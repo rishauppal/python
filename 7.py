f1ef91a5919a67f69b2750d429e52bddb7415140
rishauppal <rishauppal2@gmail.com>
2024-06-27 16:18:16 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,9 @@
+n=7  
+alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]"  
+index=0  
+for i in range(1, n + 1):
+    print(" "*(n - i), end=" ")
+    for j in range(i):
+        print(alphabet[index], end=" ")
+        index+=1      
+    print()
