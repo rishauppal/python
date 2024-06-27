a27b47b27eaf3946dfeaf9638b904ccc1a01bdbe
rishauppal <rishauppal2@gmail.com>
2024-06-27 16:33:56 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,8 +1,8 @@
 n=7  
 alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]"  
 index=0  
-for i in range(1, n + 1):
-    print(" "*(n - i), end=" ")
+for i in range(1, n+1):
+    print(" "*(n-i), end=" ")
     for j in range(i):
         print(alphabet[index], end=" ")
         index+=1      
