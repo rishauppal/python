312c639ab7fd50b3bc35c61aab820b9ef0db9658
rishauppal <rishauppal2@gmail.com>
2024-06-27 15:23:53 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+n=int(input("enter a number:"))
+if n%5==0 and n%7==0:
+    print("number is divisible by 5 and 7")
+else:
+    print("number is not divisible by 5 and 7")
+
+m=int(input("enter a number:"))
+if m%10==0 and m%56==0:
+    print("number is divisible by 10 and 56")
+else:
+    print("number is not divisible by 10 and 56")   
